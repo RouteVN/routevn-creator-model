cbb4ca0b4cf8f57ec9b16c37fc24366d976a46ef
han4wluc <han4wluc@yuusoft.com>
2026-04-11 08:23:37 +0800

    fix: add runtime field model support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ export {
   validateState,
 } from "./model.js";
 export {
+  RUNTIME_FIELD_GROUPS,
+  RUNTIME_FIELD_IDS,
+  getRuntimeFieldDefinitions,
+  isRuntimeFieldId,
+} from "./runtimeFields.js";
+export {
   SYSTEM_VARIABLE_GROUPS,
   SYSTEM_VARIABLE_IDS,
   getSystemVariableDefinitions,
